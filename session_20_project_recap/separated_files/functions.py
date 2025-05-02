52fa0a9d71f2113798551afbf81f89922a01c346
Eman <relelrify@gmail.com>
2025-05-02 22:46:07 +0300

    session 20

---
+++ session_20_project_recap/separated_files/functions.py
@@ -0,0 +1,15 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    x = add(1, 2)
+    print(x)
+
+    
+
+
+main()
---
+++ session_20_project_recap/separated_files/main.py
@@ -0,0 +1,8 @@
+from functions import *
+
+
+def jnkjsdn():
+    pass
+
+
+print(add(1, 2))
